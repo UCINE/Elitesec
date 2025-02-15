6844fcb4f1642d51f671532e690b209eb6b4f655
Mohsine Hadaoui <89942327+m0hs1ne@users.noreply.github.com>
2025-02-15 20:29:27 +0000

    feat: integrate Vercel Analytics and Speed Insights into the layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export default async function RootLayout({
   children,
@@ -13,6 +16,8 @@ export default async function RootLayout({
       </head>
       <body className="scroll-smooth antialiased [font-feature-settings:'ss01']">
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
